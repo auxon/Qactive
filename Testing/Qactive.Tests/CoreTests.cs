6b13b2f9f623741abf87ff68e3630bb76d35d225
Dave Sexton <dave@davesexton.com>
2016-05-22 11:20:43 -0400

    - Simplified core tests by using an IObservable<T> as the source. - Defined a failing SelectAnonymousType test. (Known issue #8)

--- Testing/Qactive.Tests/CoreTests.cs
+++ Testing/Qactive.Tests/CoreTests.cs
@@ -11,16 +11,16 @@ namespace Qactive.Tests
     [TestMethod]
     public async Task Infrastructure()
     {
-      var service = TestService.Create(OnNext(0), OnCompleted<int>());
+      var service = TestService.Create(Observable.Return(123));
       var results = await service.QueryAsync(xs => xs);
 
-      results.AssertEqual(service.Notifications);
+      results.AssertEqual(OnNext(123), OnCompleted<int>());
     }
 
     [TestMethod]
     public async Task Where()
     {
-      var service = TestService.Create(OnNext(0), OnNext(1), OnNext(2), OnNext(3), OnNext(4), OnCompleted<int>());
+      var service = TestService.Create(Observable.Range(0, 5));
       var results = await service.QueryAsync(xs => from x in xs
                                                    where x % 2 == 0
                                                    select x);
@@ -31,11 +31,21 @@ namespace Qactive.Tests
     [TestMethod]
     public async Task Select()
     {
-      var service = TestService.Create(OnNext(0), OnNext(1), OnNext(2), OnNext(3), OnNext(4), OnCompleted<int>());
+      var service = TestService.Create(Observable.Range(0, 5));
       var results = await service.QueryAsync(xs => from x in xs
                                                    select x * 5);
 
       results.AssertEqual(OnNext(0), OnNext(5), OnNext(10), OnNext(15), OnNext(20), OnCompleted<int>());
+    }
+
+    [TestMethod]
+    public async Task SelectAnonymousType()
+    {
+      var service = TestService.Create(QbservableServiceOptions.Unrestricted, Observable.Return(123));
+      var results = await service.QueryAsync(xs => from x in xs
+                                                   select new { Value = x });
+
+      results.AssertEqual(OnNext(new { Value = 123 }), OnCompleted(new { Value = default(int) }));
     }
   }
 }
