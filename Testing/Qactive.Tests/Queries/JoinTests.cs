27d4be6cebf10f831df577e3c629772e7481a302
Dave Sexton <dave@davesexton.com>
2016-05-22 11:21:18 -0400

    Defined simple tests for the Join operator. (passing)

--- Testing/Qactive.Tests/Queries/JoinTests.cs
+++ Testing/Qactive.Tests/Queries/JoinTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Qactive.Tests
 {
@@ -6,8 +11,39 @@ namespace Qactive.Tests
   public class JoinTests : TestBase
   {
     [TestMethod]
-    public void Join()
+    public async Task JoinClosure()
     {
+      var service = TestService.Create(QbservableServiceOptions.Unrestricted, Observable.Return(new TestContext()));
+      var range3To7 = Observable.Range(3, 5);
+
+      var results = await service.QueryAsync(source => from context in source
+                                                       from x in context.Range0To5
+                                                       join y in range3To7
+                                                       on Observable.Never<Unit>() equals Observable.Never<Unit>()
+                                                       where x == y
+                                                       select x + y);
+
+      results.AssertEqual(OnNext(6), OnNext(8), OnNext(10), OnCompleted<int>());
+    }
+
+    [TestMethod]
+    public async Task JoinInternal()
+    {
+      var service = TestService.Create(QbservableServiceOptions.Unrestricted, Observable.Return(new TestContext()));
+
+      var results = await service.QueryAsync(source => from context in source
+                                                       from x in context.Range0To5
+                                                       join y in Observable.Range(3, 7)
+                                                       on Observable.Never<Unit>() equals Observable.Never<Unit>()
+                                                       where x == y
+                                                       select x + y);
+
+      results.AssertEqual(OnNext(6), OnNext(8), OnNext(10), OnCompleted<int>());
+    }
+
+    private sealed class TestContext
+    {
+      public IObservable<int> Range0To5 => Observable.Range(0, 6);
     }
   }
 }
