4fd774574d3a4beababc552558c3de0a4ce58041
Dave Sexton <dave@davesexton.com>
2016-05-22 23:15:56 -0400

    Fixed security exception related to tracing expressions.

--- Source/Qactive/Log - Expressions.cs
+++ Source/Qactive/Log - Expressions.cs
@@ -1,19 +1,21 @@
 ﻿using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Linq.Expressions;
+using System.Security.Permissions;
+using Qactive.Properties;
 
 namespace Qactive
 {
   static partial class Log
   {
-    [Conditional("TRACE")]
+    [Conditional("TRACE"), SecurityPermission(SecurityAction.Demand, Unrestricted = true)]
     public static void ClientSendingExpression(object clientId, Expression expression)
     {
       Contract.Requires(expression != null);
 
       if (QactiveTraceSources.QactiveExpressions.Switch.ShouldTrace(TraceEventType.Information))
       {
-        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ClientSendingExpression, TraceEventType.Information, clientId, "Client sending expression", GetDebugView(expression));
+        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ClientSendingExpression, TraceEventType.Information, clientId, LogMessages.ClientSendingExpression, GetDebugView(expression));
       }
 
 #if DEBUG
@@ -21,14 +23,14 @@ namespace Qactive
 #endif
     }
 
-    [Conditional("TRACE")]
+    [Conditional("TRACE"), SecurityPermission(SecurityAction.Demand, Unrestricted = true)]
     public static void ClientRewrittenExpression(object clientId, Expression expression)
     {
       Contract.Requires(expression != null);
 
       if (QactiveTraceSources.QactiveExpressions.Switch.ShouldTrace(TraceEventType.Verbose))
       {
-        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ClientRewrittenExpression, TraceEventType.Verbose, clientId, "Client rewritten expression", GetDebugView(expression));
+        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ClientRewrittenExpression, TraceEventType.Verbose, clientId, LogMessages.ClientRewrittenExpression, GetDebugView(expression));
       }
 
 #if DEBUG
@@ -36,14 +38,14 @@ namespace Qactive
 #endif
     }
 
-    [Conditional("TRACE")]
+    [Conditional("TRACE"), SecurityPermission(SecurityAction.Demand, Unrestricted = true)]
     public static void ServerReceivingExpression(object clientId, Expression expression)
     {
       Contract.Requires(expression != null);
 
       if (QactiveTraceSources.QactiveExpressions.Switch.ShouldTrace(TraceEventType.Information))
       {
-        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ServerReceivingExpression, TraceEventType.Information, clientId, "Server receiving expression", GetDebugView(expression));
+        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ServerReceivingExpression, TraceEventType.Information, clientId, LogMessages.ServerReceivingExpression, GetDebugView(expression));
       }
 
 #if DEBUG
@@ -51,14 +53,14 @@ namespace Qactive
 #endif
     }
 
-    [Conditional("TRACE")]
+    [Conditional("TRACE"), SecurityPermission(SecurityAction.Demand, Unrestricted = true)]
     public static void ServerRewrittenExpression(object clientId, Expression expression)
     {
       Contract.Requires(expression != null);
 
       if (QactiveTraceSources.QactiveExpressions.Switch.ShouldTrace(TraceEventType.Verbose))
       {
-        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ServerRewrittenExpression, TraceEventType.Verbose, clientId, "Server rewritten expression", GetDebugView(expression));
+        QactiveTraceSources.QactiveExpressions.SemanticObject(SemanticTrace.ServerRewrittenExpression, TraceEventType.Verbose, clientId, LogMessages.ServerRewrittenExpression, GetDebugView(expression));
       }
 
 #if DEBUG
