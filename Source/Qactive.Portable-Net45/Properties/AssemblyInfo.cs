da992eeb70194a69cef9c43861374e36ea2ed8d1
Dave Sexton <dave@davesexton.com>
2016-05-23 08:08:49 -0400

    Added empty portable library targeting .NET 4.5, ASP.NET Core 1.0, Windows 8, Windows Phone 8.1, and Xamarin Android/iOS/iOS Classic.

---
+++ Source/Qactive.Portable-Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Qactive")]
+[assembly: AssemblyDescription("Qactive queryable observable providers infrastructure.")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
