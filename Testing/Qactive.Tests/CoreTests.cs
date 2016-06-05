d99027e2a2548a7361494fbda5f7387f81266d38
Dave Sexton <dave@davesexton.com>
2016-06-05 12:50:43 -0400

    Improved/simiplified a couple of new tests regarding anonymous types and arrays.

--- Testing/Qactive.Tests/CoreTests.cs
+++ Testing/Qactive.Tests/CoreTests.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using Microsoft.Reactive.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Qactive.Tests
@@ -15,7 +14,7 @@ namespace Qactive.Tests
       var service = TestService.Create(Observable.Return(123));
       var results = await service.QueryAsync(xs => xs);
 
-      results.AssertEqual(OnNext(123), OnCompleted<int>());
+      AssertEqual(results, OnNext(123), OnCompleted<int>());
     }
 
     [TestMethod]
@@ -25,7 +24,7 @@ namespace Qactive.Tests
       var results = await service.QueryAsync(xs => from x in xs
                                                    select new { Value = x });
 
-      results.AssertEqual(OnNext(new { Value = 123 }), OnCompleted(new { Value = default(int) }));
+      AssertEqual(results, OnNext(new { Value = 123 }), OnCompleted(new { Value = default(int) }));
     }
 
     [TestMethod]
@@ -35,11 +34,11 @@ namespace Qactive.Tests
       var results = await service.QueryAsync(xs => from x in xs
                                                    select new { Value = x, ValueDoubled = x * 2 });
 
-      results.AssertEqual(OnNext(new { Value = 123, ValueDoubled = 246 }), OnCompleted(new { Value = default(int), ValueDoubled = default(int) }));
+      AssertEqual(results, OnNext(new { Value = 123, ValueDoubled = 246 }), OnCompleted(new { Value = default(int), ValueDoubled = default(int) }));
     }
 
     [TestMethod]
-    public async Task AnonymousTypeInSource()
+    public async Task AnonymousTypeInSubQueryArray()
     {
       var service = TestService.Create(TestService.DuplexOptions, new[] { typeof(EnumerableEx) }, Observable.Return(123));
 
@@ -47,37 +46,24 @@ namespace Qactive.Tests
         from _ in source
         select new[] { new { Value = "test" } }.First());
 
-      results.AssertEqual(
+      AssertEqual(results,
         OnNext(new { Value = "test" }),
         OnCompleted(new { Value = default(string) }));
     }
 
     [TestMethod]
-    public async Task AnonymousTypeInSubQuery()
+    public async Task AnonymousTypeInSubQueryProjection()
     {
       var service = TestService.Create(TestService.DuplexOptions, new[] { typeof(EnumerableEx) }, Observable.Return(new[] { 123 }));
 
       var results = await service.QueryAsync(source =>
         from values in source
-        select values.Publish(published => new[] { new { Value = "test" } }).First());
+        select (from value in values
+                from result in new[] { new { Value = "test" } }
+                select result)
+                .First());
 
-      results.AssertEqual(
-        OnNext(new { Value = "test" }),
-        OnCompleted(new { Value = default(string) }));
-    }
-
-    [TestMethod]
-    public async Task LiteralSourceInSubQuery()
-    {
-      var service = TestService.Create(TestService.DuplexOptions, new[] { typeof(EnumerableEx) }, Observable.Return(123));
-
-      var results = await service.QueryAsync(source =>
-        from _ in source
-        select new[] { 123 }
-              .Publish(published => new[] { new { Value = "test" } })
-              .First());
-
-      results.AssertEqual(
+      AssertEqual(results,
         OnNext(new { Value = "test" }),
         OnCompleted(new { Value = default(string) }));
     }
