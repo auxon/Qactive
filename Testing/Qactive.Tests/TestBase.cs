4b97047baea81055875603a5d82a14fdc1b0b433
Dave Sexton <dave@davesexton.com>
2016-06-09 22:22:51 -0400

    Improved TestBase.AssertEqual to compare Exceptions by count, index, type (assignable-from) and exact message.

--- Testing/Qactive.Tests/TestBase.cs
+++ Testing/Qactive.Tests/TestBase.cs
@@ -26,9 +26,52 @@ namespace Qactive.Tests
 
     public static void AssertEqual<T>(IEnumerable<Notification<T>> actual, IEnumerable<Notification<T>> expected)
     {
-      if (expected.All(e => e.Kind != NotificationKind.OnError))
+      var expectedErrors = expected.Where(n => n.Kind == NotificationKind.OnError).Select((n, index) => new { Error = n.Exception, index }).ToList();
+
+      if (expectedErrors.Count == 0)
       {
         AssertNoOnError(actual);
+      }
+      else
+      {
+        var actualErrors = actual.Where(n => n.Kind == NotificationKind.OnError).Select((n, index) => new { Error = n.Exception, index }).ToList();
+
+        Func<string> buildMessageDetails = () =>
+            "Actual Errors: " + Environment.NewLine + string.Join(Environment.NewLine, actualErrors.Select(x => x.index + ": " + x.Error.Message)) + Environment.NewLine
+          + "Expected Errors: " + Environment.NewLine + string.Join(Environment.NewLine, expectedErrors.Select(x => x.index + ": " + x.Error.Message));
+
+        if (actualErrors.Count != expectedErrors.Count)
+        {
+          Assert.Fail($"Actual error count ({actualErrors.Count}) does not equal the expected error count ({expectedErrors.Count})." + Environment.NewLine + buildMessageDetails());
+        }
+        else
+        {
+          var pairs = actualErrors.Zip(expectedErrors, (x, y) => new { Actual = x, Expected = y });
+
+          if (!pairs.All(pair => pair.Actual.index == pair.Expected.index))
+          {
+            Assert.Fail($"Actual errors are not in the same positions in the sequence as the expected errors." + Environment.NewLine + buildMessageDetails());
+          }
+          else if (!pairs.All(pair => pair.Expected.Error.GetType().IsAssignableFrom(pair.Actual.Error.GetType()) && pair.Expected.Error.Message.Equals(pair.Actual.Error.Message)))
+          {
+            Assert.Fail($"Actual errors are not equal to the expected errors." + Environment.NewLine + buildMessageDetails());
+          }
+          else
+          {
+            var list = actual as IList<Notification<T>>;
+
+            if (list == null || list.IsReadOnly)
+            {
+              list = actual.ToList();
+              actual = list;
+            }
+
+            foreach (var pair in pairs)
+            {
+              list[pair.Actual.index] = OnError<T>(pair.Expected.Error);
+            }
+          }
+        }
       }
 
       actual.AssertEqual(expected);
