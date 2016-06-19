4a8b0a2fe2c5d37813487181788ab24904033341
Dave Sexton <dave@davesexton.com>
2016-06-19 01:17:01 -0400

    Added .NET 4.0 flavor for Qactive.Providers.Tcp.

---
+++ Source/Qactive.Providers.Tcp-Net40/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Qactive.Providers.Tcp")]
+[assembly: AssemblyDescription("Qactive TCP provider for client and server.")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
